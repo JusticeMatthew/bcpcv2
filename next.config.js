04da67429fef0fa683bb1980896ffb26056485f0
matthew <matthewajustice@gmail.com>
2023-05-10 21:51:17 -0400

    andy wasn't fixed, but maybe this will fix andy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['main.d9gcazq6ff2zg.amplifyapp.com', 'bandcpestcontrol.com'],
-  },
   reactStrictMode: true,
   i18n: {
     locales: ['en'],
